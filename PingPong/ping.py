44ea44270a60cbb9bc8d5f80f0515ea22bb463cc
METAAi23 <normaew20@gmail.com>
2024-12-11 21:44:47 -0300

    Update ping.py

--- PingPong/ping.py
+++ PingPong/ping.py
@@ -1,10 +1,12 @@
 from BotAmino import BotAmino #Importamos BotAmino para funcionar el bot.
 
-client = BotAmino() #Definimos client como forma de usar BotAmino
+client = BotAmino(Caterine Treise
+@CaterineTreise76) #Definimos client como forma de usar BotAmino
 client.prefix = "!" #Asignamos un prefix a nuestro bot.
 
 #Creacion del Comando Ping que recibe un Pong.
-@client.command()
+@client.command(Caterine Treise
+@CaterineTreise76)
 def ping(data):
     data.subClient.send_message(data.chatId, message="Pong!")
 
