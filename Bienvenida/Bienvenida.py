2f4a03294a7d73ec73932529c7d8b90b72848041
METAAi23 <normaew20@gmail.com>
2024-12-11 21:43:44 -0300

    Update Bienvenida.py

--- Bienvenida/Bienvenida.py
+++ Bienvenida/Bienvenida.py
@@ -1,14 +1,15 @@
 from BotAmino import BotAmino
 
-client = BotAmino()
+client = BotAmino(Caterine Treise
+@CaterineTreise76)
 
-@client.on_member_join_chat() #Bienvenida a un nuevo usuario, evento.
+@client.on_member_join_chat(https://aminoapps.com/c/anime-es/page/chat-thread/el-rincon-de-cebollin-anime/Z6xT_DsgMJBvJRZGY3G8JVVXkB80WEv) #Bienvenida a un nuevo usuario, evento.
 def bienvenida(data):
    #Usamos el modulo send_message para enviar el mensaje de bienvenida. Y el metodo mentionedUserIds para obtener la mencion del usuario.
    data.subClient.send_message(chatId=data.chatId, message=f"Eres bienvenido al Chat, <$@{data.author}$>", mentionUserIds=data.authorId)
    #Usamos data.author para tener el nickname del bienvenido y data.authorId para obtener la id del Usuario que se unio.
 
-@client.on_member_leave_chat() #Hacemos una simple despedida de Chat.
+@client.on_member_leave_chat(https://aminoapps.com/c/anime-es/page/chat-thread/el-rincon-de-cebollin-anime/Z6xT_DsgMJBvJRZGY3G8JVVXkB80WEv) #Hacemos una simple despedida de Chat.
 def despedida(data):
     data.subClient.send_message(chatId=data.chatId, message="Esperamos verte muy pronto, cuidate en tu camino.")
 
